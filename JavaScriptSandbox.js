f03e0df318f7db699e7c11537851a9f149d4f8ba
taylorhayduk <taylorhayduk@gmail.com>
2015-07-14 17:14:35 -0400

    PI correction

--- JavaScriptSandbox.js
+++ JavaScriptSandbox.js
@@ -7,11 +7,12 @@ function handleOrientation(event) {
   var alpha    = event.alpha;
   var beta     = event.beta;
   var gamma    = event.gamma;
+  alphaInRadians = alpha/(Math.PI*2);
 
   document.getElementById("absolute").innerHTML = absolute;
   document.getElementById("alpha").innerHTML = alpha;
   document.getElementById("beta").innerHTML = beta;
   document.getElementById("gamma").innerHTML = gamma;
+  document.getElementById("alphaInRadians").innerHTML = alphaInRadians;
   // Do stuff with the new orientation data
-  alphaInRadians = alpha/(Math.pi*2);
 }
