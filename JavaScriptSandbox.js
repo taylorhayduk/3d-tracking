9128758d089fc21c9ec084711e92c0825eb6ea93
taylorhayduk <taylorhayduk@gmail.com>
2015-07-14 01:21:20 -0400

    deviceOrientation
    test0

--- JavaScriptSandbox.js
+++ JavaScriptSandbox.js
@@ -1,1 +1,14 @@
-alert('Hello, World.  I\' workin on GitHub!!!')
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+function handleOrientation(event) {
+  var absolute = event.absolute;
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
+
+  document.getElementById("absolute").innerHTML = absolute;
+  document.getElementById("alpha").innerHTML = alpha;
+  document.getElementById("beta").innerHTML = beta;
+  document.getElementById("gamma").innerHTML = gamma;
+  // Do stuff with the new orientation data
+}
