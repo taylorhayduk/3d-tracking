62ba706432e6e5cf9ecc1047e1a16469fae4242f
taylorhayduk <taylorhayduk@gmail.com>
2015-07-14 17:29:22 -0400

    alpha over 180

--- JavaScriptSandbox.js
+++ JavaScriptSandbox.js
@@ -7,7 +7,7 @@ function handleOrientation(event) {
   var alpha    = event.alpha;
   var beta     = event.beta;
   var gamma    = event.gamma;
-  alphaInRadians = alpha/(Math.PI*360);
+  alphaInRadians = alpha/(180);
 
   document.getElementById("absolute").innerHTML = absolute;
   document.getElementById("alpha").innerHTML = alpha;
